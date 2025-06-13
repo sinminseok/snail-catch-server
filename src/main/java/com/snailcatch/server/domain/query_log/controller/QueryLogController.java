fea74054907594dca69caa9c8eb282f8bb12af8c
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-06-14 03:19:51 +0900

    loki 로그 수집 환경 구성 및 도커 컨테이너 배포 환경 구축

--- src/main/java/com/snailcatch/server/domain/query_log/controller/QueryLogController.java
+++ src/main/java/com/snailcatch/server/domain/query_log/controller/QueryLogController.java
@@ -1,21 +1,13 @@
 package com.snailcatch.server.domain.query_log.controller;
 
-import com.snailcatch.server.domain.query_log.dto.QueryLogCursorRequest;
 import com.snailcatch.server.domain.query_log.dto.QueryLogCursorResponse;
 import com.snailcatch.server.domain.query_log.dto.QueryLogRequest;
-import com.snailcatch.server.domain.query_log.dto.QueryLogResponse;
 import com.snailcatch.server.domain.query_log.service.BufferedQueryLogService;
 import com.snailcatch.server.domain.query_log.service.QueryLogService;
-import com.snailcatch.server.global.dto.PaginationResponse;
 import com.snailcatch.server.global.dto.SuccessResponse;
 import com.snailcatch.server.global.annotation.ApiKey;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- start.sh
+++ start.sh
@@ -1,5 +1,20 @@
-echo "🛠 Building JAR with Gradle..."
-./gradlew clean build
+#!/bin/bash
 
-echo "🐳 Building Docker containers..."
-docker-compose up --build
+APP_IMAGE="minseok1768/snail-app:latest"
+
+echo "🧹 Removing local docker image if exists..."
+docker rmi -f $APP_IMAGE || echo "No existing local image to remove."
+
+echo "🛠 Building JAR with Gradle..."
+./gradlew clean build -x test  # 테스트 실패로 인한 빌드 중단 방지
+
+echo "🐳 Building Docker image..."
+docker build -t $APP_IMAGE .
+
+echo "🚀 Pushing Docker image to Docker Hub..."
+docker push $APP_IMAGE
+
+echo "🔧 Starting containers with docker-compose..."
+docker-compose up -d --build
+
+echo "✅ All done!"
